d6a4f0c11328405c039d485c8bb934f4abddc8d5
Jakub Slowiak <slowiakjakub@gmail.com>
2023-11-22 22:48:19 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const PORT = require('./config');
+
+const app = express();
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server listening on PORT: ${PORT}`)
+})
+
+app.get("/status", (request,response) => {
+    const status = {
+        "Status": "Running"
+    };
+
+    response.send(status);
+});
