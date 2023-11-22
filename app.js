792f74b799283c809ed6a7c04dc35fbf76aa6337
Jakub Slowiak <slowiakjakub@gmail.com>
2023-11-23 00:53:19 +0100

    Add songs route and mock CRUD operations

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const PORT = require('./config');
+const songsRouter = require('./routes/songs');
 
 const app = express();
 app.use(express.json());
+app.use('/songs',songsRouter);
 
 app.listen(PORT, () => {
     console.log(`Server listening on PORT: ${PORT}`)
 })
 
 app.get("/status", myFunction);
+
+
+
+
 
 function myFunction (request,response) {
     const status = {
