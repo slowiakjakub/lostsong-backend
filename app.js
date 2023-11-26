10c3050239d74a03aafdfc1a8bb080bb112c5500
Jakub Slowiak <slowiakjakub@gmail.com>
2023-11-26 19:01:35 +0100

    Add origins back, without extra slash for vercel

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const mongoose = require('mongoose');
 const Song = require('./models/song');
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: ['https://lostsong-frontend-app.vercel.app','http://localhost:5173']
+}));
 
 app.use(express.json());
 
