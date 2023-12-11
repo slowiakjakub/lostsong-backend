6703ca3053062700c9a5f3b9c25733b8fc69b95d
Jakub Slowiak <slowiakjakub@gmail.com>
2023-12-11 23:56:43 +0100

    Add songs/top endpoint

--- controllers/songController.js
+++ controllers/songController.js
@@ -6,6 +6,29 @@ exports.getAllSongs = (req, res) => {
     .catch((err) => res.status(500).json({ message: err.message }));
 };
 
+exports.getTopSongs = (req, res) => {
+  Song.aggregate([
+    {
+      $group: {
+        _id: { title: "$title", artist: "$artist" },
+        occurrenceCount: { $sum: 1 }
+      }
+    },
+    { $sort: { occurrenceCount: -1 } },
+    { $limit: 5 },
+    {
+      $project: {
+        _id: 0,
+        title: "$_id.title",
+        artist: "$_id.artist",
+        occurrenceCount: 1
+      }
+    }
+  ])
+  .then((songs) => res.json(songs))
+  .catch((err) => res.status(500).json({ message: err.message }));
+};
+
 exports.addSong = (req, res) => {
   const newSong = new Song(req.body);
   newSong
--- routes/songRoutes.js
+++ routes/songRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const songController = require('../controllers/songController');
 
 router.get('/',songController.getAllSongs);
+router.get('/top', songController.getTopSongs);
 router.post('/',songController.addSong);
 
 module.exports = router;
